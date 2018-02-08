1268d3d9f3d3ac744a2aae0336b0e3d6f2419a0b
hao3304 <hao3304@qq.com>
2018-02-08 18:09:00 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+<style>
+html,body {
+  padding: 0;
+  margin: 0;
+  overflow: hidden;
+  height: 100%;
+  font-family: 'Helvetica Neue',Helvetica,'PingFang SC','Hiragino Sans GB','Microsoft YaHei',Arial,sans-serif;
+  color: rgb(85, 85, 85);
+  font-weight: 400;
+}
+#app {
+  height: 100%;
+}
+</style>
---
+++ src/views/login.vue
@@ -0,0 +1,127 @@
+<template>
+    <div class="login">
+        <section class="login-block">
+                <h5>爱生活，爱编程</h5>
+                <ul class="tab">
+                    <li class="active">
+                        <a href="javascript:;">登录</a>
+                    </li>
+                    <li>
+                        <a href="javascript:;">注册</a>
+                    </li>
+                </ul>
+                <div class="form">
+                    <input type="text" placeholder="请输入您的用户名">
+                    <input type="text" placeholder="请输入您的密码">
+                </div>
+                <button class="submit">
+                    登录
+                </button>
+        </section>
+        <vue-particles color="#fff"></vue-particles>
+    </div>
+</template>
+<script>
+export default {
+  name: 'login'
+}
+</script>
+<style lang="less">
+    .login {
+        height: 100%;
+        position: relative;
+        background: url("~/static/images/bg.jpg") no-repeat;
+
+        .login-block{
+            text-align: center;
+            background-color: rgba(255,255,255,.9);
+            position: absolute;
+            width: 300px;
+            border-radius: 5px;
+            left: 50%;
+            margin-left: -150px;
+            box-sizing: border-box;
+            padding: 0 10px;
+            top: 50%;
+            margin-top: -200px;
+            h5{
+                font-size: 18px;
+                font-weight: 400;
+                color: #555;
+            }
+
+            ul.tab {
+                padding: 0;
+                li {
+                    list-style: none;
+                    display: inline-block;
+                    width: 75px;
+                    a{
+                        display: block;
+                        text-decoration: none;
+                        text-align: center;
+                        color: #555;
+                        opacity: .7;
+                        line-height: 35px;
+                        transition:opacity .15s,color .15s;
+                        position: relative;
+                    }
+
+                    &.active {
+                        a{
+                            color: #0f88eb;
+                            opacity: 1;
+                            &:after{
+                                content: ' ';
+                                position: absolute;
+                                bottom:0;
+                                left: 50%;
+                                width:40px;
+                                height:2px;
+                                background-color: #0f88eb;
+                                margin-left: -20px;
+                            }
+                        }
+                    }
+                }
+            }
+
+            .form{
+                border:1px solid #d5d5d5;
+                border-radius: 3px;
+                background: rgba(255,255,255,0.5);
+                input {
+                    display: block;
+                    width: 100%;
+                    border:none;
+                    color: #666;
+                    font-family: 'Microsoft Yahei';
+                    padding: 1em .8em;
+                    box-sizing: border-box;
+
+                    &:first-child{
+                        border-bottom: 1px solid #ddd;
+                    }
+                }
+            }
+
+            .submit{
+                    width: 100%;
+                    border:none;
+                    margin: 20px 0;
+                    background-color: #0f88eb;
+                    border-radius: 3px;
+                    line-height: 41px;
+                    color: #fff;
+                    box-shadow: none;
+                    font-family: 'Microsoft Yahei';
+                    font-size: 15px;
+                    cursor: pointer;
+
+                    &:hover {
+                        background-color: #2d96ec;
+                    }
+            }
+        }
+    }
+</style>
